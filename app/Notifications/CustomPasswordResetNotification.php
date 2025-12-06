d98fa894b165ef677653e330488a963eb93815d2
Anonymous <anon@example.com>
2025-12-06 11:08:16 +0200

    fixing reset password url sent to inbox

--- app/Notifications/CustomPasswordResetNotification.php
+++ app/Notifications/CustomPasswordResetNotification.php
@@ -44,10 +44,10 @@ class CustomPasswordResetNotification extends Notification
         return (new MailMessage)
             ->subject('Reset Password Notification')
             ->line('You are receiving this email because we received a password reset request for your account.')
-            ->action('Reset Password', url(config('app.frontend_url').route('password.reset', [
+            ->action('Reset Password', route('password.reset', [
                 'token' => $this->token,
                 'email' => $notifiable->getEmailForPasswordReset()
-            ], false)))
+            ], absolute: true))
             ->line('This password reset link will expire in '.config('auth.passwords.'.config('auth.defaults.passwords').'.expire').' minutes.')
             ->line('If you did not request a password reset, no further action is required.');
     }
